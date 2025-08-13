54f955d6f4550ada8bdea4f64f4b697b8676614a
Paul Madeley <paul.madeley@owlfarmltd.com>
2025-08-13 12:29:52 +0000

    Should we be simplifying the sell form if it's causing so many issues? S

--- src/components/sell-form.tsx
+++ src/components/sell-form.tsx
@@ -18,7 +18,7 @@ import { ListingPreview } from './listing-preview';
 import { handleGenerateListingDetails, handlePolishDescription } from '@/lib/actions';
 import { useActionState } from 'react';
 import { Textarea } from './ui/textarea';
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from './ui/accordion';
+import { GeneralInformation } from './sell/general-information';
 
 const formSchema = z.object({
   title: z.string().optional(),
@@ -385,70 +385,7 @@ export function SellForm() {
               </CardContent>
             </Card>
 
-            <Card>
-              <CardHeader>
-                <CardTitle>General Information</CardTitle>
-                <CardDescription>Provide the main dimensions and details of the yacht.</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <Accordion type="multiple" defaultValue={['dimensions', 'hull-details']} className="w-full">
-                  <AccordionItem value="dimensions">
-                    <AccordionTrigger>Dimensions & Core Details</AccordionTrigger>
-                    <AccordionContent className="pt-4">
-                      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                        <FormField control={form.control} name="model" render={({ field }) => (<FormItem><FormLabel>Model</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="boatType" render={({ field }) => (<FormItem><FormLabel>Type</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="loaM" render={({ field }) => (<FormItem><FormLabel>LOA (m)</FormLabel><FormControl><Input type="number" {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="lwlM" render={({ field }) => (<FormItem><FormLabel>LWL (m)</FormLabel><FormControl><Input type="number" {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="beamM" render={({ field }) => (<FormItem><FormLabel>Beam (m)</FormLabel><FormControl><Input type="number" {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="draftM" render={({ field }) => (<FormItem><FormLabel>Draft (m)</FormLabel><FormControl><Input type="number" {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="airDraftM" render={({ field }) => (<FormItem><FormLabel>Air Draft (m)</FormLabel><FormControl><Input type="number" {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="headroomM" render={({ field }) => (<FormItem><FormLabel>Headroom (m)</FormLabel><FormControl><Input type="number" {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="year" render={({ field }) => (<FormItem><FormLabel>Year Built</FormLabel><FormControl><Input type="number" {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="make" render={({ field }) => (<FormItem><FormLabel>Builder</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="country" render={({ field }) => (<FormItem><FormLabel>Country</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="designer" render={({ field }) => (<FormItem><FormLabel>Designer</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="displacementT" render={({ field }) => (<FormItem><FormLabel>Displacement (t)</FormLabel><FormControl><Input type="number" {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="ballastTonnes" render={({ field }) => (<FormItem><FormLabel>Ballast (tonnes)</FormLabel><FormControl><Input type="number" {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                      </div>
-                    </AccordionContent>
-                  </AccordionItem>
-                  <AccordionItem value="hull-details">
-                     <AccordionTrigger>Hull, Deck & Keel</AccordionTrigger>
-                     <AccordionContent className="pt-4">
-                       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                        <FormField control={form.control} name="hullMaterial" render={({ field }) => (<FormItem><FormLabel>Hull Material</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="hullColor" render={({ field }) => (<FormItem><FormLabel>Hull Colour</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="hullShape" render={({ field }) => (<FormItem><FormLabel>Hull Shape</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="keelType" render={({ field }) => (<FormItem><FormLabel>Keel Type</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="superstructureMaterial" render={({ field }) => (<FormItem><FormLabel>Superstructure Material</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="deckMaterial" render={({ field }) => (<FormItem><FormLabel>Deck Material</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="deckFinish" render={({ field }) => (<FormItem><FormLabel>Deck Finish</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="superstructureDeckFinish" render={({ field }) => (<FormItem><FormLabel>Superstructure Deck Finish</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                        <FormField control={form.control} name="cockpitDeckFinish" render={({ field }) => (<FormItem><FormLabel>Cockpit Deck Finish</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                       </div>
-                     </AccordionContent>
-                  </AccordionItem>
-                   <AccordionItem value="other-general">
-                     <AccordionTrigger>Other General Details</AccordionTrigger>
-                     <AccordionContent className="pt-4">
-                       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                          <FormField control={form.control} name="dorades" render={({ field }) => (<FormItem><FormLabel>Dorades</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                          <FormField control={form.control} name="windowFrame" render={({ field }) => (<FormItem><FormLabel>Window Frame</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                          <FormField control={form.control} name="windowMaterial" render={({ field }) => (<FormItem><FormLabel>Window Material</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                          <FormField control={form.control} name="deckhatch" render={({ field }) => (<FormItem><FormLabel>Deckhatch</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                          <FormField control={form.control} name="fuelTankLitre" render={({ field }) => (<FormItem><FormLabel>Fuel Tank (litre)</FormLabel><FormControl><Input type="number" {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                          <FormField control={form.control} name="levelIndicatorFuel" render={({ field }) => (<FormItem><FormLabel>Level Indicator (Fuel)</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                          <FormField control={form.control} name="freshwaterTankLitre" render={({ field }) => (<FormItem><FormLabel>Freshwater Tank (litre)</FormLabel><FormControl><Input type="number" {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                          <FormField control={form.control} name="levelIndicatorFreshwater" render={({ field }) => (<FormItem><FormLabel>Level Indicator (Freshwater)</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                          <FormField control={form.control} name="wheelSteering" render={({ field }) => (<FormItem><FormLabel>Wheel Steering</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                          <FormField control={form.control} name="outsideHelmPosition" render={({ field }) => (<FormItem><FormLabel>Outside Helm Position</FormLabel><FormControl><Input {...field} value={field.value ?? ''} /></FormControl><FormMessage /></FormItem>)} />
-                       </div>
-                     </AccordionContent>
-                  </AccordionItem>
-                </Accordion>
-              </CardContent>
-            </Card>
+            <GeneralInformation form={form} />
             
             <Card>
                 <CardHeader>
